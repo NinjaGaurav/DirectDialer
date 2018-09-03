854e4be4abbd1733ee4529c20d6caf0020018df0
Gaurav Bholla <gbholla@gmail.com>
2018-09-03 23:12:25 +0530

    FINAL

---
+++ app/src/main/java/gaurav/bholla/directdialer/MainActivity.java
@@ -0,0 +1,235 @@
+package gaurav.bholla.directdialer;
+
+import android.Manifest;
+import android.content.ContentProviderOperation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.Menu;
+import android.telephony.SmsManager;
+
+
+import com.gauravbholla.dd.AutoCompleteContactTextView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    //textView=editText
+
+    private final int PICK_CONTACT = 1;
+    AutoCompleteContactTextView textView;
+    EditText editText2;
+    EditText editText3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView =  findViewById(R.id.coolStuff);
+        editText3 = findViewById(R.id.enter_message);
+        editText2 =  findViewById(R.id.enter_name);
+        int Permission_All = 1;
+        String[] Permissions = {Manifest.permission.SEND_SMS, Manifest.permission.CALL_PHONE, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CONTACTS,Manifest.permission.WRITE_CONTACTS};
+        if(!hasPermissions(this, Permissions)){
+            ActivityCompat.requestPermissions(this, Permissions, Permission_All);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_phone:
+                textView.setType(AutoCompleteContactTextView.TYPE_OF_DATA.PHONE);
+                item.setChecked(true);
+                break;
+            case R.id.menu_mail:
+                item.setChecked(true);
+                textView.setType(AutoCompleteContactTextView.TYPE_OF_DATA.EMAIL);
+                break;
+            case R.id.menu_both:
+                item.setChecked(true);
+                textView.setType(AutoCompleteContactTextView.TYPE_OF_DATA.BOTH);
+                break;
+            case R.id.bold:
+                if (item.isChecked()){
+                    item.setChecked(false);
+                    textView.changeStyle(AutoCompleteContactTextView.STYLE.NONE);
+                }
+                else{
+                    item.setChecked(true);
+                    textView.changeStyle(AutoCompleteContactTextView.STYLE.BOLD);
+                }
+        }
+        return true;
+    }
+
+    public boolean hasPermissions(Context context, String... permissions){
+
+        if(context!=null && permissions!=null){
+            for(String permission: permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission)!=PackageManager.PERMISSION_GRANTED){
+                    return  false;
+                }
+            }
+        }
+        return true;
+    }
+ /*   public void callContacts(View v)
+    {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        switch (reqCode) {
+            case (PICK_CONTACT) :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c =  managedQuery(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+
+                        String id =c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+
+                        String hasPhone =c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                        if (hasPhone.equalsIgnoreCase("1")) {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,
+                                    null, null);
+                            phones.moveToFirst();
+                            String cNumber = phones.getString(phones.getColumnIndex("data1"));
+
+                            Intent callIntent = new Intent(Intent.ACTION_CALL);
+
+                            callIntent.setData(Uri.parse("tel:"+cNumber));
+
+                            startActivity(callIntent);
+
+                        }
+
+                    }
+                }
+                break;
+        }
+    }
+    */
+    public void clear(View view)
+    {
+        textView.setText("");
+        editText2.setText("");
+        editText3.setText("");
+
+    }
+    public void phoneCall(View view) {
+
+        String number = textView.getText().toString();
+        if(number.length()<1)
+            Toast.makeText(this, "Enter Phone Number!", Toast.LENGTH_LONG).show();
+        else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+
+            callIntent.setData(Uri.parse("tel:" + number));
+
+            startActivity(callIntent);
+        }
+    }
+
+    public void autoSMS(View view)
+    {
+        SmsManager sm = SmsManager.getDefault();
+        String number = textView.getText().toString();
+        String msg = editText3.getText().toString();
+
+        if(msg.length()<1 && number.length()<1)
+            Toast.makeText(this, "Enter Phone Number & Message!", Toast.LENGTH_LONG).show();
+        else if(msg.length()<1)
+            Toast.makeText(this, "Enter Message!", Toast.LENGTH_LONG).show();
+        else if(number.length()<1)
+            Toast.makeText(this, "Enter Phone Number!", Toast.LENGTH_LONG).show();
+        else{
+            sm.sendTextMessage(number, null, msg, null, null);
+            Toast.makeText(this, "Message Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+    public void addAsContactAutomatic(View view) {
+        String displayName = editText2.getText().toString();
+        String mobileNumber = textView.getText().toString();
+        Context context = this;
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        // Names
+        if(displayName.length()<1 || mobileNumber.length()<1)
+        {
+            if(displayName.length()<1 && mobileNumber.length()<1)
+                Toast.makeText(context, "Enter Name & Phone Number!", Toast.LENGTH_LONG).show();
+            else if(displayName.length()<1)
+                Toast.makeText(context, "Enter Name!", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(context, "Enter Phone Number!", Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            if (displayName != null) {
+                ops.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                                displayName).build());
+            }
+
+            // Mobile Number
+            if (mobileNumber != null) {
+                ops.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
+            }
+
+
+            // Asking the Contact provider to create a new contact
+            try {
+                context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(context, "Contact " + displayName + " added.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
---
+++ contactsautocompletetextview/src/main/java/com/gauravbholla/dd/AutoCompleteContactTextView.java
@@ -0,0 +1,377 @@
+package com.gauravbholla.dd;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.telephony.PhoneNumberUtils;
+import android.text.Editable;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by laurentmeyer on 02/03/15.
+ */
+
+/**
+ * Main class of the lib
+ */
+public class AutoCompleteContactTextView extends AutoCompleteTextView implements CustomAdapter.AsyncLoad {
+    Context context;
+    int colorData;
+    int colorName;
+    boolean typedLettersShouldBeDifferent;
+    String returnPattern;
+    People selected = null;
+    boolean hasCustomAdapter = false;
+    CustomAdapter adapter;
+    String typedLetterStyle;
+    AttributeSet attrs;
+    int xmlIntType = 0;
+
+    /**
+     * Choose which data you want to have
+     */
+    public enum TYPE_OF_DATA {
+        PHONE, EMAIL, BOTH
+    }
+
+    public enum STYLE {NONE, BOLD, UNDERLINE}
+
+    /**
+     * Call it if you want to create this view programmatically
+     *
+     * @param s: Style you want to set refer to {@link com.gauravbholla.dd.AutoCompleteContactTextView.STYLE}
+     */
+    public void changeStyle(STYLE s) {
+        switch (s) {
+            case NONE:
+                typedLettersShouldBeDifferent = false;
+                break;
+            case BOLD:
+                typedLettersShouldBeDifferent = true;
+                typedLetterStyle = "b";
+                break;
+            case UNDERLINE:
+                typedLettersShouldBeDifferent = true;
+                typedLetterStyle = "i";
+                break;
+        }
+    }
+
+    // Default is phone
+    private TYPE_OF_DATA type = TYPE_OF_DATA.PHONE;
+
+    boolean displayPhoto;
+
+    public AutoCompleteContactTextView(Context context) {
+        super(context);
+        this.context = context;
+        init(context, null, false);
+    }
+
+    public AutoCompleteContactTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, false);
+    }
+
+    public AutoCompleteContactTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, false);
+    }
+
+    private void init(Context context, AttributeSet attrs, boolean programmatic) {
+        this.context = context;
+        this.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (AutoCompleteContactTextView.this.getAdapter() != null) {
+                    ((CustomAdapter) AutoCompleteContactTextView.this.getAdapter()).getFilter().filter(s);
+                }
+                selected = null;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        this.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selected = (People) getAdapter().getItem(position);
+                setText(selected.getData().toString());
+            }
+        });
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.PhoneNumberAutoComplete);
+            colorData = array.getColor(R.styleable.PhoneNumberAutoComplete_colorOfData, getResources().getColor(android.R.color.holo_blue_dark));
+            colorName = array.getColor(R.styleable.PhoneNumberAutoComplete_colorOfNames, getResources().getColor(android.R.color.black));
+            typedLettersShouldBeDifferent = array.getBoolean(R.styleable.PhoneNumberAutoComplete_typedLettersHaveDifferentStyle, false);
+            if (typedLettersShouldBeDifferent) {
+                typedLetterStyle = array.getInt(R.styleable.PhoneNumberAutoComplete_styleOfTypedLetters, 2) == 1 ? "u" : "b";
+            }
+            if (!programmatic) {
+                xmlIntType = array.getInt(R.styleable.PhoneNumberAutoComplete_typeOfData, 1);
+                switch (xmlIntType) {
+                    case 1:
+                        type = TYPE_OF_DATA.PHONE;
+                        break;
+                    case 2:
+                        type = TYPE_OF_DATA.EMAIL;
+                        break;
+                    case 3:
+                        type = TYPE_OF_DATA.BOTH;
+                        break;
+                }
+            }
+            returnPattern = TextUtils.isEmpty(array.getString(R.styleable.PhoneNumberAutoComplete_getTextPattern)) ? "[Nn]: [P]" : array.getString(R.styleable.PhoneNumberAutoComplete_getTextPattern);
+            displayPhoto = array.getBoolean(R.styleable.PhoneNumberAutoComplete_displayPhotoIfAvailable, false);
+            array.recycle();
+        }
+        adapter = new ContactsAdapter(context, type);
+        this.setAdapter(adapter);
+    }
+
+    /**
+     * Should give the possibility to people to change the basic adapter but NOT TESTED!
+     *
+     * @param adapter: adapter to be set
+     */
+    // TODO: Try to see if it works
+    public void setCustomAdapter(CustomAdapter adapter) {
+        this.adapter = adapter;
+        hasCustomAdapter = true;
+        setAdapter(adapter);
+    }
+
+    @Override
+    public void hasLoaded(CustomAdapter adapter) {
+        setAdapter(adapter);
+    }
+
+    @Override
+    public Editable getText() {
+        if (isSomeoneSelected()) {
+            String temp = returnPattern;
+            String name = selected.getName().toString();
+            String data = selected.getData().toString();
+            if (temp.contains("[Nn]")) {
+                boolean isAdapted = Character.isUpperCase(name.toCharArray()[0]);
+                if (!isAdapted) {
+                    char[] array = name.toCharArray();
+                    array[0] = Character.toUpperCase(array[0]);
+                    name = String.valueOf(array);
+                }
+                temp = temp.replace("[Nn]", name);
+            }
+            if (temp.contains("[N]")) {
+                temp = temp.replace("[N]", name.toUpperCase());
+            }
+            if (temp.contains("[n]")) {
+                temp = temp.replace("[n]", name.toLowerCase());
+            }
+            if (temp.contains("[d]")) {
+                temp = temp.replace("[d]", data);
+            }
+            return Editable.Factory.getInstance().newEditable(temp);
+        }
+        return super.getText();
+    }
+
+    public String getName() {
+        if (isSomeoneSelected()) {
+            return selected.getName().toString();
+        }
+        return null;
+    }
+
+    public String getData() {
+        if (isSomeoneSelected()) {
+            return selected.getData().toString();
+        }
+        return null;
+    }
+
+    /**
+     * Example of a possible implementation of the {@link CustomAdapter}
+     */
+    private class ContactsAdapter extends CustomAdapter {
+
+        ArrayList<People> toDisplayList = new ArrayList<>();
+        Filter filter;
+
+        private ContactsAdapter(Context context) {
+            super(context, type, AutoCompleteContactTextView.this);
+        }
+
+        public ContactsAdapter(Context context, TYPE_OF_DATA type) {
+            super(context, type, AutoCompleteContactTextView.this);
+        }
+
+        @Override
+        public int getCount() {
+            return toDisplayList == null ? 0 : toDisplayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return toDisplayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v;
+            if (convertView == null) {
+                v = inflater.inflate(R.layout.layout_cell, null);
+            } else {
+                v = convertView;
+            }
+            TextView name = (TextView) v.findViewById(R.id.cell_name);
+            TextView data = (TextView) v.findViewById(R.id.cell_data);
+            name.setText(((People) getItem(position)).getName());
+            name.setTextColor(colorName);
+            data.setText(((People) getItem(position)).getData());
+            data.setTextColor(colorData);
+            if (displayPhoto) {
+                ImageView iv = (ImageView) v.findViewById(R.id.thumbnail_picture);
+                People p = (People) getItem(position);
+                if (p.getPicture() != null) {
+                    iv.setImageBitmap(p.getPicture());
+                    iv.setVisibility(View.VISIBLE);
+                }
+            }
+            return v;
+        }
+
+        @Override
+        public Filter getFilter() {
+            filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults r = new FilterResults();
+                    if (constraint == null || constraint.length() == 0) {
+                        r.values = dataList;
+                        r.count = (dataList == null ? 0 : dataList.size());
+                    } else {
+                        ArrayList<People> filtered = new ArrayList<>();
+                        for (People toFilter : dataList) {
+                            if (toFilter.getName().toString().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                                if (typedLettersShouldBeDifferent) {
+                                    Spanned text = setBold(constraint, toFilter);
+                                    toFilter.setName(text);
+                                }
+                                filtered.add(toFilter);
+                            }
+                            String data = toFilter.getData().toString();
+                            boolean isPhoneNumber = PhoneNumberUtils.isGlobalPhoneNumber(data);
+                            if (isPhoneNumber) {
+                                String phone = toFilter.getData().toString().toLowerCase().replaceAll("\\D", "");
+                                if (phone.indexOf(constraint.toString().toLowerCase()) != -1) {
+                                    filtered.add(toFilter);
+                                }
+                            } else {
+                                if (toFilter.getData().toString().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                                    filtered.add(toFilter);
+                                }
+                            }
+                            // TODO: Make it country code insensible
+                            // One approach but with limitations:
+                            /*
+                                It does work when you type "0176", it looks for "176" which is contained in +49176....
+                             */
+                            if (constraint.toString().startsWith("0") && !constraint.toString().startsWith("00")) {
+                                if (toFilter.getData().toString().toLowerCase().contains(constraint.toString().toLowerCase().substring(1))) {
+                                    filtered.add(toFilter);
+                                }
+                            }
+                        }
+                        r.values = filtered;
+                        r.count = filtered.size();
+                    }
+                    return r;
+                }
+
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    toDisplayList = (ArrayList<People>) results.values;
+                    notifyDataSetChanged();
+                }
+            };
+            return filter;
+        }
+
+        /**
+         * It's maybe the most complicated function of this lib (yes, this lib has a trivial logic!)
+         * It's just a loop which add to all occurrences of a string a <[letter]></[letter]> to be
+         * displayed as bold or underlined by the #TextView (because rendered to HTML)
+         *
+         * @param constraint:                    The substring we want to highlight
+         * @param nameOfPeopleWeWantToHighlight: The people object on which these modifications will be done.
+         * @return a highlighted TextView compatible string
+         */
+        private Spanned setBold(CharSequence constraint, People nameOfPeopleWeWantToHighlight) {
+            String temp = nameOfPeopleWeWantToHighlight.getName().toString().replace("<" + typedLetterStyle + ">", "").replace("</" + typedLetterStyle + ">", "");
+            ArrayList<Integer> positions = new ArrayList<>();
+            for (int i = -1; (i = temp.toLowerCase().indexOf(constraint.toString().toLowerCase(), i + 1)) != -1; ) {
+                positions.add(i);
+            }
+            StringBuilder builder = new StringBuilder(temp);
+            int offsetIntroduced = 0;
+            for (Integer position : positions) {
+                builder.insert(position + (offsetIntroduced * 7), "<" + typedLetterStyle + ">");
+                builder.insert(position + ((offsetIntroduced * 7) + 3) + constraint.length(), "</" + typedLetterStyle + ">");
+                offsetIntroduced++;
+            }
+            return Html.fromHtml(builder.toString());
+        }
+
+    }
+
+    public TYPE_OF_DATA getType() {
+        return type;
+    }
+
+    public void setType(TYPE_OF_DATA type) {
+        this.type = type;
+        clear();
+        init(context, attrs, true);
+    }
+
+    private void clear() {
+        setText("");
+    }
+
+    /**
+     * Used just to see what the getText should return.
+     *
+     * @return if someone is selected or not
+     */
+    public boolean isSomeoneSelected() {
+        return selected != null;
+    }
+}
